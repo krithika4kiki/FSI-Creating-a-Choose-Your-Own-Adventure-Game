eb840e73945e8070e6191e4578bebc29594ed38c
Your Name <keerthikrish002@gmail.com>
2022-03-23 16:42:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Your Code here
+let answer = window.prompt('Do you head left or right?')
+
+console.log(answer)
+
+if (answer === 'left'){
+    console.log('user entered left')
+}else if (answer === 'right'){
+    console.log('user entered right')
+}else {
+    console.log('user entered something else')
+}
